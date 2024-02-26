fa6859125ab3f893d16e4dd738a9fbe8dfc815db
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-02-25 21:49:44 -0500

    first commit

---
+++ first_project/main.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+
+from flask import url_for
+from flask import request
+
+
+app = Flask(__name__)
+
+def valid_login(username, password):
+    return username == 'admin' and password == 'password'
+
+def log_the_user_in(username):
+    return f'Hello {escape(username)}'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
